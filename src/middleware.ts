5d9ae089d8fe22eb3e860994ef56c498d748ce88
shokiogawa <shouki2578@gmail.com>
2023-12-14 23:45:01 +0900

    delete image

---
+++ src/middleware.ts
@@ -0,0 +1,24 @@
+import { NextRequest, NextResponse } from "next/server";
+
+function middleware1(request: NextRequest) {
+  const url = request.url;
+  console.log("middleware1 =>", { url });
+
+  return NextResponse.next();
+}
+
+function middleware2(request: NextRequest) {
+  const pathname = request.nextUrl.pathname;
+  console.log("middleware2 =>", { pathname });
+
+  return NextResponse.next();
+}
+
+export function middleware(request: NextRequest) {
+  middleware1(request);
+  middleware2(request);
+}
+
+export const config = {
+  matcher: ["/"],
+};
